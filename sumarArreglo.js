94006fa156803c570e84acae71730d3454c5988a
Irvin Guerrero Sanchez <irvings349@gmail.com>
2024-02-27 12:08:53 -0600

    Ejercicio 11

---
+++ sumarArreglo.js
@@ -0,0 +1,12 @@
+// escribe tu respuesta acá
+function sumarArreglo(arreglo) {
+    var suma = 0;
+    for (let i = 0; i < arreglo.length; i++) {
+        suma += arreglo[i];
+    }
+    return suma;
+}
+// código de prueba
+console.log(sumarArreglo([3, 1, 2])) // 6
+console.log(sumarArreglo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 55
+console.log(sumarArreglo([])) // 0
