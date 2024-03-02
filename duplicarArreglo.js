dcad9b65ee1debe44cb0d36eaafa4dda061d3a19
Irvin Guerrero Sanchez <irvings349@gmail.com>
2024-03-01 22:55:48 -0600

    Ejercicio 23

---
+++ duplicarArreglo.js
@@ -0,0 +1,14 @@
+// escribe tu función acá
+function duplicar(arreglo) {
+    if (arreglo == []) {
+        return [];
+    }
+    var arregloDuplicado = [];
+    for (let i = 0; i < arreglo.length; i++) {
+        arregloDuplicado[i] = arreglo[i] * 2;
+    }
+    return arregloDuplicado;
+}
+// código de prueba
+console.log(duplicar([1, 2, 3])) // [2, 4, 6]
+console.log(duplicar([])) // []
