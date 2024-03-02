d3a4c537d536a46c6a9c0e2e280507cc6bd7075e
Irvin Guerrero Sanchez <irvings349@gmail.com>
2024-03-01 23:16:23 -0600

    Ejercicio 24

---
+++ empiezaConA.js
@@ -0,0 +1,18 @@
+// escribe tu función acá
+function empiezanConA(arreglo) {
+    if (arreglo == []) {
+        return [];
+    }
+    var arregloDeA = [];
+    for (let i = 0; i < arreglo.length; i++) {
+        if (arreglo[i][0] == 'a' || arreglo[i][0] == "A") {
+            arregloDeA.push(arreglo[i]);
+        }
+    }
+    return arregloDeA;
+}
+// código de prueba
+console.log(empiezanConA(["beta", "alfa", "Arbol", "gama"])) // ["alfa", "Arbol"]
+console.log(empiezanConA(["Arduino", "alfa", "Arbol", "gama"])) // ["alfa", "Arbol"]
+console.log(empiezanConA(["beta", "delta", "gama"])) // []
+console.log(empiezanConA([])) // []
