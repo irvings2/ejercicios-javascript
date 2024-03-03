7cf1bcd3c4c275f8556f626e8157d6233a35450e
Irvin Guerrero Sanchez <irvings349@gmail.com>
2024-03-02 21:56:02 -0600

    Ejercicio 25

---
+++ terminaConS.js
@@ -0,0 +1,18 @@
+// escribe tu función acá
+function terminanConS(arreglo) {
+    if (arreglo == []) {
+        return [];
+    }
+    var terminanConS = [];
+    for (let i = 0; i < arreglo.length; i++) {
+        if (arreglo[i][arreglo[i].length-1] == "s" || arreglo[i][arreglo[i].length-1] == "S") {
+            terminanConS.push(arreglo[i]);
+        }
+    }
+    return terminanConS;
+}
+// código de prueba
+console.log(terminanConS(["pruebas", "arroz", "árbol", "tokens"])) // ["pruebas", "tokens"]
+console.log(terminanConS(["pruebas", "arroz", "árboles", "tokens"])) // ["pruebas", "arboles", "tokens"]
+console.log(terminanConS(["beta", "delta", "gama"])) // []
+console.log(terminanConS([])) // []
